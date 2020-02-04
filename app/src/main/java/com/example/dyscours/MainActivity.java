f47ce00a093c1b119f74dccb5cb44a1b2b5f108a
kyeoul <insertwinterhere@gmail.com>
2020-02-04 12:30:37 -0600

    toolbar broke a g a i n with the same error as last time

--- app/src/main/java/com/example/dyscours/MainActivity.java
+++ app/src/main/java/com/example/dyscours/MainActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        super.onCreate(savedInstanceState);
+
+        ArrayList<Debate> debateList = new ArrayList<>();
+        debateList.add(new Debate("i would like flame war plz", "jeneric"));
+        debateList.add(new Debate("plz roastt me", "wfaieufhwoieufwhef"));
+
+        ListView listView = (ListView) findViewById(R.id.debateList);
+
+        ListviewAdapter adapter = new ListviewAdapter(getApplicationContext(), debateList);
+        listView.setAdapter(adapter);
     }
 }
